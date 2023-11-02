c8fc0ef3025cfeca5746c8873d02efdb38508646
guilhermehilario <guilhermehilariosilva@gmail.com>
2023-11-02 19:52:33 -0300

    create styled Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,6 @@
-export function Button() {
-  return;
+import { Container } from "./styles";
+import { ButtonType } from "./types";
+
+export function Button(props: ButtonType) {
+  return <Container {...props}> </Container>;
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,3 +1,9 @@
-import styled from "sytled-components";
+import styled from "styled-components";
 
-export const Container = styled.button``;
+export const Container = styled.button`
+  background-color: ${({ theme }) => theme.colors.primary};
+  width: 128px;
+  height: 12px;
+  padding: 16px 24px 16px 24px;
+  border-radius: 12px;
+`;
