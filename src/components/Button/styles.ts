b2d476e5f7cf5732230d4300f14a2bfc14371a1a
guilhermehilario <guilhermehilariosilva@gmail.com>
2023-11-16 14:57:42 -0300

    refacoty button component

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
   background-color: ${({ theme }) => theme.colors.primary};
   width: 128px;
   height: 12px;
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,3 +1,1 @@
-export type ButtonType = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  label: string;
-};
+export type ButtonType = React.ButtonHTMLAttributes<HTMLButtonElement>;
