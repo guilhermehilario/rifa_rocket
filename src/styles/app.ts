15effc023c18c7861da4a5486d019e3dd660e164
guilhermehilario <guilhermehilariosilva@gmail.com>
2023-11-16 15:50:24 -0300

    adding title and  refactoring page styled component

--- src/styles/app.ts
+++ src/styles/app.ts
@@ -2,7 +2,19 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+
   background-color: ${({ theme }) => theme.colors.background};
-  padding: 12px 24px;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const Title = styled.text`
+  font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  font-size: 32px;
+  font-weight: 700;
+  padding: 0;
+  margin: 24px 0;
 `;
