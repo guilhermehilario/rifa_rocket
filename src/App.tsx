82b1fbc1e3a518a1ced1adea8adbfe4e722101b5
guilhermehilario <guilhermehilariosilva@gmail.com>
2023-11-16 14:36:01 -0300

    create view app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from "styled-components";
 import { darkTheme } from "./theme";
 import "./styles/reset.css";
 import { Container } from "./styles/app";
+import { CreateRifaView } from "./pages/Create/create-rifa-view";
 
 export function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <Container>
         <h1> Rifa Rocket </h1>
+        <CreateRifaView />
       </Container>
     </ThemeProvider>
   );
